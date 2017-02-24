e820fe100b6785d7c0d051a61f9b999154e930a3
Annie <abvolatile@gmail.com>
2017-02-24 14:22:56 -0800

    add range drill and instructions

---
+++ PythonDrill-range.py
@@ -0,0 +1,27 @@
+#
+# PYTHON:   version 3.6.0
+#
+# AUTHOR:   Annie Bowman
+#
+# PURPOSE:  Tech Academy Python Course (Item 46)
+#           Python Range Function DRILL 
+#
+
+
+#1
+for x in range(4):
+    print(x)
+    
+print()
+
+#2
+for y in range(3, -1, -1):
+    print(y, end=" ")
+    
+print()
+print()
+
+#3
+for z in range(8, 0, -2):
+    print(z, end=" ")
+
